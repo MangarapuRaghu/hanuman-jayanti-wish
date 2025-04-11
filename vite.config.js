d0e832138e0fde2fa7d5beebbceafb0963b5c2e3
Manarapu Raghu <162829469+MangarapuRaghu@users.noreply.github.com>
2025-04-11 23:00:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  plugins: [react()],
+  root: '.',            // Ensure root is current directory
+  build: {
+    outDir: 'dist'
+  }
+})
